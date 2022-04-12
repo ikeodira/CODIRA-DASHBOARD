a30bac09b6f6bca586d93ea35e0e21db25618552
ikeodira <84882009+ikeodira@users.noreply.github.com>
2022-04-12 16:02:18 -0700

    Add files via upload

---
+++ CODIRA DASHBOARD/orders.js
@@ -0,0 +1,62 @@
+const Orders = [
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Larvender KF102 Drone",
+        productNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: "Ruko Fill Pro Drone",
+        productNumber: '45631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Drone with Camera Drone",
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: "GPS 4k Drone",
+        productNumber: '82231',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: "Foldable Mini Drone",
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Larvender KF102 Drone",
+        productNumber: '85631',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: "Ruko Fill Pro Drone",
+        productNumber: '45631',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: "Drone with Camera Drone",
+        productNumber: '85631',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: "GPS 4k Drone",
+        productNumber: '82231',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
